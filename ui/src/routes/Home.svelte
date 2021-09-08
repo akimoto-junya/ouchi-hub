c69f7c839d7e0951445b22ca58c6960f382f0458
Takachiha <akimoto14301@gmail.com>
2021-09-08 20:02:17 +0900

    [update] Homeでlibraryにリダイレクトするよう変更

--- ui/src/routes/Home.svelte
+++ ui/src/routes/Home.svelte
@@ -1,34 +1,7 @@
 <script>
+  import {onMount} from 'svelte';
   import {push} from "svelte-spa-router";
-  let name = "Home";
-  const navigateWork1 = () => {
-    push("/works/1");
-  };
+
+  onMount(() => push('/library'));
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <button on:click={navigateWork1}>Work 1</button>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
